ef22d8553fad425163855add65337c0ac70c4e33
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-09-17 12:26:54 -0300

    feat: Creating the AdapterConcrets

--- Adapter/AdapterConcrets/Game16BitAdapter.h
+++ Adapter/AdapterConcrets/Game16BitAdapter.h
@@ -5,4 +5,24 @@
 #ifndef GAME16BITADAPTER_H
 #define GAME16BITADAPTER_H
 
+#include "Adapter.h"
+#include <iostream>
+using namespace std;
+
+class Game16BitAdapter : public Adapter {
+    Game16Bit& game;
+public:
+    Game16BitAdapter(Game16Bit& game) : game(game) {}
+
+    void moveLeft() override {
+        game.movePlayer("left");
+    }
+    void moveRight() override {
+        game.movePlayer("right");
+    }
+    void moveUp() override {
+        game.playerJump();
+    }
+};
+
 #endif //GAME16BITADAPTER_H
--- Adapter/AdapterConcrets/Game8BitAdapter.h
+++ Adapter/AdapterConcrets/Game8BitAdapter.h
@@ -5,4 +5,25 @@
 #ifndef GAME8BITADAPTER_H
 #define GAME8BITADAPTER_H
 
+#include "Adapter.h"
+
+#include <iostream>
+using namespace std;
+
+class Game8BitAdapter : public Adapter {
+    Game8Bit& game;
+public:
+    Game8BitAdapter(Game8Bit& game) : game(game) {}
+
+    void moveLeft() override {
+        game.moveCharacter("left");
+    }
+    void moveRight() override {
+        game.moveCharacter("right");
+    }
+    void moveUp() override {
+        game.characterJump();
+    }
+};
+
 #endif //GAME8BITADAPTER_H
