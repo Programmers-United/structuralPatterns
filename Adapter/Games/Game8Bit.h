7870f53b54ed6188f72c8a0a866b7efe157b8e2b
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-09-17 12:27:34 -0300

    feat: Creating the interface for old games

--- Adapter/Games/Game8Bit.h
+++ Adapter/Games/Game8Bit.h
@@ -5,4 +5,17 @@
 #ifndef GAME8BIT_H
 #define GAME8BIT_H
 
+#include <iostream>
+using namespace std;
+
+class Game8Bit {
+public:
+    void moveCharacter(const string& direction) {
+        cout << "8-bit character moving\n" << direction << endl;
+    }
+    void characterJump() {
+        cout << "8-bit character jumping\n" << endl;
+    }
+};
+
 #endif //GAME8BIT_H
